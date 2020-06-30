7c702df36e75008e63daa1d610e2d9656c08819d
thibault <thibault.grandjean@icloud.com>
2020-06-30 10:48:05 +0200

    Can download data automatically

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,19 +1,72 @@
 # -*- coding: utf-8 -*-
+import getpass
+import os
 import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+from zipfile import ZipFile
+
+try:
+    import kaggle
+except OSError:
+    print("Kaggle API's credential required")
+    username = input("kaggle username : ")
+    key = getpass.getpass("kaggle API key")
+    os.environ['KAGGLE_USERNAME'] = username
+    os.environ['KAGGLE_KEY'] = key
+    import kaggle
+
+from src.features import build_features
+
+# Get data from kaggle
+DATASET_NAME = "home-credit-default-risk"
+
+
+def init_data_dir(project_dir):
+    """Create empty dir if they not exists"""
+    logger = logging.getLogger(__name__)
+    logger.info("Init data directories")
+    filepath = Path(project_dir).resolve()
+    data_dirs = ['external',
+                 'interim',
+                 'processed',
+                 'raw']
+    data_path = filepath.joinpath('data')
+    if not os.path.exists(data_path):
+        os.mkdir(data_path)
+        for directory in data_dirs:
+            os.mkdir(data_path.joinpath(directory))
+    else:
+        logger.info('already exists')
+
+
+def download_dataset(name, output_filepath, **kwargs):
+    """Download the dataset."""
+    logger = logging.getLogger(__name__)
+    logger.info("Start download, this can take a while...")
+    kaggle.api.authenticate()
+    kaggle.api.competition_download_files(name, path=output_filepath)
+
+
+def unzip_dataset(input_filepath, output_filepath, clean=True):
+    with ZipFile(input_filepath, 'r') as zip_obj:
+        zip_obj.extractall(output_filepath)
+    if clean:
+        os.remove(input_filepath)
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+@click.argument('input_filepath', type=click.Path())
+def main(input_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    input_filepath = Path(input_filepath).resolve()
+    download_dataset(DATASET_NAME, output_filepath=input_filepath)
+    unzip_dataset(input_filepath.joinpath(DATASET_NAME + '.zip'),
+                  input_filepath)
 
 
 if __name__ == '__main__':
@@ -23,8 +76,5 @@ if __name__ == '__main__':
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
+    init_data_dir(project_dir)
     main()
