cde8aa2193e6887b303306645c37bc55c48656ac
thibault <thibault.grandjean@icloud.com>
2020-07-01 12:31:08 +0200

    Handle train/test data. NB_CREDIT_ACTIVE from Bureau.
    :todo: add nb_active_cred from previous_application

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import pandas as pd
+import warnings
 
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
@@ -55,17 +56,52 @@ class FeaturesBuilder():
             raise ValueError("Path %s does not exist")
 
     def __load(self, file):
+        """Load CSV file as pandas.DataFrame."""
         return pd.read_csv(self.data_path.joinpath(file))
+
+    def __assert_dataframe(self, dataframe):
+        """Ensure data are provided as pandas dataframe.
+
+        raises : ValueError if dataframe is not a pandas.DataFrame object.
+        """
+        if type(dataframe) != pd.DataFrame:
+            raise ValueError("Invalid data format,"
+                             " should be a pandas's dataframe")
 
     @property
     def data(self):
+        """Internal representation of the features."""
         return self.__data
 
     @data.setter
     def data(self, dataframe):
-        if type(dataframe) != pd.DataFrame:
-            raise ValueError("Must be a pandas's dataframe.")
+        self.__assert_dataframe(dataframe)
+        if 'SK_ID_CURR' in dataframe.columns:
+            dataframe.set_index('SK_ID_CURR', inplace=True, drop=True)
         self.__data = dataframe
+
+    @property
+    def test_data(self):
+        """Internale representation of the features for testing purpose."""
+        return self.__test_data
+
+    @test_data.setter
+    def test_data(self, dataframe):
+        self.__assert_dataframe(dataframe)
+        if 'SK_ID_CURR' in dataframe.columns:
+            dataframe.set_index('SK_ID_CURR', inplace=True, drop=True)
+        self.__test_data = dataframe
+
+    @property
+    def full_data(self):
+        return pd.concat([self.data, self.test_data], axis=0)
+
+    @full_data.setter
+    def full_data(self, dataframe):
+        train_data = dataframe.loc[self.data.index]
+        test_data = dataframe.loc[self.test_data.index]
+        self.data = train_data
+        self.test_data = test_data
 
     @property
     def application_train(self):
@@ -73,7 +109,13 @@ class FeaturesBuilder():
         self.app_train = self.__load(self.FILES[0])
         return self.app_train[self.BASICS]
 
-    def handle_categorical_features(self, strategy='le'):
+    @property
+    def application_test(self):
+        """Return only relevant columns from the application_test file."""
+        self.app_test = self.__load(self.FILES[1])
+        return self.app_test[self.BASICS]
+
+    def handle_categorical_features(self, strategy='auto'):
         """Handle categorical data.
 
         strategy: should be one of "le" (LabelEncoder), "ohe" (OneHotEncoder)
@@ -81,11 +123,63 @@ class FeaturesBuilder():
         """
         for col in self.__data:
             if self.__data[col].dtype.name == 'category':
+                if strategy == 'le':
+                    # LabelEncoder
+                    pass
+                elif strategy == 'ohe':
+                    # OneHotEncoder
+                    pass
+                elif strategy == 'auto':
+                    # Infer
+                    pass
+                else:
+                    raise ValueError('Strategy must be one of'
+                                     ' "le", "ohe" or "auto"')
+
+    def handle_missing(self, strategy='auto'):
+        """Handle missing values.
+
+        strategy: should be "auto", "impute" or "drop"
+        """
+        for col in self.data.select_dtypes(exclude=["category", "object"]):
+            if self.data[col].dtype.name == 'int64':
+                warnings.warn('Integer data detected.'
+                             ' Data must be converted to float')
+            elif self.data[col].dtype.name == 'float64':
+                # TODO: impute
                 pass
+
+    # TODO: Finish this. (Build all features that already exists in notebooks)
+    ## Aggregated data
+    @property
+    def _history(self):
+        """Return customer's history."""
+        # Load concerned file
+
+        # Merge on SK_ID_CURR
+
+        return None
+
+    def _active_credit(self):
+        """For each appliant, we count all credits that are active."""
+        sk_id_curr = self.data.reset_index(inplace=False,
+                                           drop=False)['SK_ID_CURR']
+        bureau = self.__load(self.FILES[2])
+        # previous_application = self.__load(self.FILES[-2])
+        # previous_application.set_index('SK_ID_CURR', inplace=True)
+        active_cred_bur = bureau[bureau['CREDIT_ACTIVE'] == 'Active']
+        nb_active_cred = active_cred_bur[['SK_ID_CURR', 'CREDIT_ACTIVE']]\
+            .groupby('SK_ID_CURR').count()
+        self.full_data = self.full_data.join(nb_active_cred)
+
+    def aggregate(self):
+        """Call all function that make a feature by aggregate."""
+        self.data['history'] = self._history
 
     def build(self):
         """Main entrypoint."""
         self.data = self.application_train
+        self.test_data = self.application_test
 
 # Data loading
 def load_data(src=BASE_PATH.joinpath('data', 'processed')):
